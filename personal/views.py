bc4be82a30bca117a5ff1dd9256b710cfa186f67
LatteX <latteindigox@gmail.com>
2022-05-30 21:45:07 +0300

    intergrate account templates with views

--- personal/views.py
+++ personal/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from .models import Question
+from account.models import Account
 # Create your views here.
 # functional views
 def home_screen_view(request):
-
-
-    data_qst = Question.objects.all()
-    context ={
-        'somekeystring':"Some value to the view",
-        'some_number':12
-    }
-
-    context['questions'] =  data_qst
+    context = {}
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
     return render(request, "personal/home.html", context)
