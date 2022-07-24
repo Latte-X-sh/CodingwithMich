b0a3c611816bca462e0a4d97a5f6f83e9bd2ec39
Latte_X <latteindigox@gmail.com>
2022-07-24 03:22:22 +0300

    started dealing with pass_management

--- account/views.py
+++ account/views.py
@@ -56,8 +56,13 @@ def account_view(request):
     if request.POST:
         form = AccountUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
+            form.initial={
+                "email":request.POST['email'],
+                "username":request.POST['username']
+            }
             form.save()
-            return redirect('account')
+            context['success_message'] = "Updated"
+
     else:
         form = AccountUpdateForm(
             initial={
